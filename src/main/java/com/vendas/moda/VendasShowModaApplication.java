77a2fee80b0380c830409d657b66365f03695270
Israel Lima <israel_lima_16@hotmail.com>
2018-07-05 20:20:16 -0300

    Projeto Criado

---
+++ src/main/java/com/vendas/moda/VendasShowModaApplication.java
@@ -0,0 +1,12 @@
+package com.vendas.moda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasShowModaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasShowModaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vendas/moda/VendasShowModaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vendas.moda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VendasShowModaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
